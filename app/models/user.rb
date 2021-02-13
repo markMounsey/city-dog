6ee1e4f2231c89212dbdf7eac0221b6f22b3e888
peter huyser <huyserp@gmail.com>
2021-02-13 13:53:43 +0100

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  SIZES = ['small', 'med-small', 'medium', 'med-large', 'large']
+  validates :dog_name, presence: true
+  validates :owner_name, presence: true
+  validates :size, inclusion: { in: SIZES }
 end

bdd7b5c8c02f00cdc7d15884c456bc005629c0c2
Karl Keller <kgk86@hotmail.com>
2021-02-20 12:36:36 +0100

    removed validation on :size

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,6 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
 
-  SIZES = ['small', 'med-small', 'medium', 'med-large', 'large']
   validates :dog_name, presence: true
   validates :owner_name, presence: true
-  # validating the size causes the Devise signup to fail
-  validates :size, inclusion: { in: SIZES }
 end

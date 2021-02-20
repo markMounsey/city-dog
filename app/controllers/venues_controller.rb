d38211adfbbfadb856657e79ad6b2e5d713e20c6
peter huyser <huyserp@gmail.com>
2021-02-20 14:06:08 +0100

    shows all venues on index page with general info

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,14 @@
 class VenuesController < ApplicationController
   def index
-    @venues = policy_scope(Venue).order(created_at: :desc).includes(:tag)
+    @venues = policy_scope(Venue).order(created_at: :desc)
+    all_venues = Venue.all
+    @ratings_by_venue = all_venues.map do |venue|
+      if venue.reviews.empty?
+        []
+      else
+        venue.reviews.map { |review| review.rating }
+      end
+    end
   end
 
 

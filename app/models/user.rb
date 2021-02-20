f879a2707a833fe29a94e9617e4096233edca118
Karl Keller <kgk86@hotmail.com>
2021-02-20 12:34:56 +0100

    validating the size causes the Devise signup to fail

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,6 @@ class User < ApplicationRecord
   SIZES = ['small', 'med-small', 'medium', 'med-large', 'large']
   validates :dog_name, presence: true
   validates :owner_name, presence: true
+  # validating the size causes the Devise signup to fail
   validates :size, inclusion: { in: SIZES }
 end

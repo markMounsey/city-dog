6dc885679133ae1d280afab0b1d1b00b908ce616
Mark Mounsey <mark.mounsey0512@gmail.com>
2021-02-23 18:57:47 +0100

    refactored javascript

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,5 @@
 import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+
 
   const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
@@ -15,7 +15,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   };
 
 

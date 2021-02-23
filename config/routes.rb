04b232ade97e3b8d12c1d7c8efb4543368125653
Mark Mounsey <mark.mounsey0512@gmail.com>
2021-02-23 19:54:53 +0100

    create reviews controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/edit'
+  get 'reviews/update'
+  get 'reviews/destory'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"

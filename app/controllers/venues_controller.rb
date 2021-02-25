6ae0498df24135cf7a714f3b70a56dace86b0c9e
peter huyser <huyserp@gmail.com>
2021-02-25 18:18:52 +0100

    gave default pic in view if not provide by db, make reviews list into notifications

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -3,7 +3,7 @@ class VenuesController < ApplicationController
   def index
     @venues = policy_scope(Venue).order(created_at: :desc)
     @all_venues = Venue.all
-    @ratings_by_venue = @all_venues.map do |venue|
+    @ratings_by_venue = @all_venues.reverse.map do |venue|
       if venue.reviews.empty?
         []
       else

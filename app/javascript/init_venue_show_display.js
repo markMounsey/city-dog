947f81feb2e61351cc014fb9e6adb1494f692c65
peter huyser <huyserp@gmail.com>
2021-02-25 20:17:58 +0100

    setup JS to toggle displays on venue show

---
+++ app/javascript/init_venue_show_display.js
@@ -0,0 +1,5 @@
+const toggleDisplay = () => {
+  console.log('hellooooooooo!');
+}
+
+export { toggleDisplay };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { toggleDisplay } from '../components/init_venue_show_display';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,4 +21,5 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  toggleDisplay();
 })

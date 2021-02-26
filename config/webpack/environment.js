2855fc84c072dffefabece9e17d199262aae2cca
peter huyser <huyserp@gmail.com>
2021-02-26 10:41:15 +0100

    added environment.loaders.delete(nodeModules);

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,9 @@
 const { environment } = require('@rails/webpacker')
 // Bootstrap 4 has a dependency over jQuery & Popper.js:
 const webpack = require('webpack')
+
+environment.loaders.delete('nodeModules');
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',

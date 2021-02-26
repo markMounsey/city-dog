59c1eb2c1ca4637d5820860a0d994400364f4f98
peter huyser <huyserp@gmail.com>
2021-02-26 14:22:49 +0100

    reviews and photos both display properly when toggling

--- app/javascript/components/init_venue_show_display.js
+++ app/javascript/components/init_venue_show_display.js
@@ -6,17 +6,20 @@ const toggleDisplay = () => {
   const reviewsList = document.querySelector('.reviews');
   const photosList = document.querySelector('.photos');
 
-  reviewsBtn.focus();
+  if(reviewsBtn) {
+    reviewsBtn.focus();
 
-  photosBtn.addEventListener('click', () => {
-    reviewsList.classList.add("hidden");
-    photosList.classList.remove("hidden");
-  });
+    photosBtn.addEventListener('click', () => {
+      reviewsList.classList.add("hidden");
+      photosList.classList.remove("hidden");
+    });
 
-  reviewsBtn.addEventListener('click', () => {
-    reviewsList.classList.remove("hidden");
-    photosList.classList.add("hidden");
-  });
+    reviewsBtn.addEventListener('click', () => {
+      reviewsList.classList.remove("hidden");
+      photosList.classList.add("hidden");
+    });
+  }
+
 }
 
 export { toggleDisplay };

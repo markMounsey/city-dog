4a8afe07b2b97dfb4245ea2b4824c556ba8770c6
peter huyser <huyserp@gmail.com>
2021-02-28 16:21:13 +0100

    add validations for venuetag and tag, create route for venuetag to add tags to a venue

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   has_many :venuetags, dependent: :destroy
   has_many :venues, through: :venuetags
-  validates :name, presence: true, length: { maximum: 25 }
+  validates :name, presence: true, length: { maximum: 25 }, uniqueness: true
 end

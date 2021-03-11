f893b35c4446fda6f3670ad1879f5a15e8c45300
Karl Keller <kgk86@hotmail.com>
2021-03-11 20:40:19 +0100

    reorder the list of tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ApplicationRecord
   has_many :venues, through: :venuetags
   validates :name, presence: true, uniqueness: true
 
-  $tagnames = ["waterbowls", "loud", "dogtreats", "friendly", "warm",
-              "humanlikesfood", "crowded", "cozy", "quiet", "outside",
-              "inside", "spacious", "€", "€€", "€€€", "poochparty", "humanlikesbeer"]
+  $tagnames = ["€", "€€", "€€€", "cozy", "crowded", "dogtreats", "friendly", "humanlikesbeer",
+              "humanlikesfood", "inside", "outside", "waterbowls", "loud", "quiet",
+              "warm", "spacious", "poochparty"]
 end
